5265bb7bc3bde1b8b7b6ce8cea994d2267d6a9b8
Glider Mapalad <gmapa002@bender.engr.ucr.edu>
2025-06-10 15:18:27 -0700

    Having trouble with spacing

--- naive_K_search.py
+++ naive_K_search.py
@@ -1,1 +1,24 @@
 #implement best k search
+
+import numpy as np
+from collections import Counter
+from naive_knn import predict_knn
+
+def naive_bestKsearch(x_train, y_train, y_val, krange): 
+	BEST_K = -1
+	BEST_ACCURACY = -1
+	BEST_RANGED_K = -1
+	BEST_RANGED_ACCURACY = -1
+	
+	print("\n--- NAIVE FIND BEST K ---")
+	#loop through all k values and find the best 
+	for k in krange:
+		correct = 0
+		ranged_correct = 0
+		
+		pred = predict_knn(X_train, Y_train, X_val[i], k)
+       		actual = Y_val[i]
+       		if pred == actual:
+           		correct += 1
+       		if abs(pred - actual) <= 1:
+           		ranged_correct += 1
