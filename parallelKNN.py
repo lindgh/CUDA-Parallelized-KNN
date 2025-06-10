94ac6fd1ef3fd615bb5a3c8a70e941c6eeaeb904
Glider Mapalad <gmapa002@bender.engr.ucr.edu>
2025-06-09 20:07:43 -0700

    started loop which calls cosine kernel

--- parallelKNN.py
+++ parallelKNN.py
@@ -12,4 +12,12 @@ def numbaParallelKNN(x_train, y_train, x_val, y_val, k = 3):
 
 	train_d = cuda.to_device(x_train) 
 	
-	
+	#for each validation review, get cosine distance from each training tf-idf vector
+	for i in range(len(x_val)): 	
+		testVector = x_val[i:i+1] #the review to test cosine distance
+		test_d = cuda.to_device(testVector)
+		distances_d = cuda.device_array(x_train.shape[0],dtype=np.float32)
+
+		#print("does this even work")
+
+		
